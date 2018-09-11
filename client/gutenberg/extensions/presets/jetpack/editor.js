2dd6c5e2dcbdd5f4eed22fdf3fbb29f85af9691b
Marin Atanasov <tyxla@abv.bg>
2018-09-11 15:29:36 +0300

    Shorten block import in preset

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -4,5 +4,5 @@
  * Internal dependencies
  */
 import 'gutenberg/extensions/markdown/jetpack-markdown-block.jsx';
-import 'gutenberg/extensions/related-posts/index.js';
+import 'gutenberg/extensions/related-posts';
 import 'gutenberg/extensions/tiled-gallery/tiled-gallery.jsx';
