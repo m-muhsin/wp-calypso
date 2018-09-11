8a3ac7302c1a39f160e85cb8629d5a932ed9ba90
Marin Atanasov <tyxla@abv.bg>
2018-09-11 12:17:19 +0300

    Add Related Posts to Jetpack preset

--- client/gutenberg/extensions/presets/jetpack/editor.js
+++ client/gutenberg/extensions/presets/jetpack/editor.js
@@ -4,4 +4,5 @@
  * Internal dependencies
  */
 import 'gutenberg/extensions/markdown/jetpack-markdown-block.jsx';
+import 'gutenberg/extensions/related-posts/index.js';
 import 'gutenberg/extensions/tiled-gallery/tiled-gallery.jsx';
