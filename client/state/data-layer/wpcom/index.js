d13044dffd1a13b9425f3bf182aed8c2abff2edf
Dennis Snell <dennis.snell@automattic.com>
2018-09-11 03:12:50 -0400

    hog me

--- client/state/data-layer/wpcom/index.js
+++ client/state/data-layer/wpcom/index.js
@@ -5,10 +5,9 @@
  */
 
 import { mergeHandlers } from 'state/action-watchers/utils';
-import me from './me';
 import read from './read';
 import sites from './sites';
 
-export const handlers = mergeHandlers( me, read, sites );
+export const handlers = mergeHandlers( read, sites );
 
 export default handlers;
